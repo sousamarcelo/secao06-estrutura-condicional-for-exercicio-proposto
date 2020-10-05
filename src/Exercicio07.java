bcf84ff8229d188c3fa0cfaadc4a509c400f9e30
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-05 18:03:42 -0300

    Exercicio07 - ao quadrado e ao cubo aplicando cast

---
+++ src/Exercicio07.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+//começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+//exemplo.
+
+public class Exercicio07 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			
+			int posicao1 = i;
+			int posicao2 = (int)Math.pow(i, 2);
+			int posicao3 = (int)Math.pow(i, 3);
+			
+			System.out.printf("%d %d %d%n", posicao1, posicao2, posicao3);		
+		}
+		
+		sc.close();
+
+	}
+
+}
