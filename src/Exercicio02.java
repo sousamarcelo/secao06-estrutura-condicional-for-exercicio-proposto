65228b6a2aef20f9b0777382e41aa54bb8184b40
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-05 13:03:53 -0300

    Exercicio-02

---
+++ src/Exercicio02.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+//Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+//Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+//essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um valor: ");
+		int valor = sc.nextInt();
+		
+		int in = 0;
+		int out = 0;
+		
+		for (int i = 0; i < valor; i++) {
+			int numero = sc.nextInt();
+			if (numero >= 10 && numero <= 20) {
+				in++;
+			} else {
+				out++;
+			}
+		}
+		
+		System.out.println(in + " in");
+		System.out.println(out + " out");
+		
+		sc.close();
+
+	}
+
+}
